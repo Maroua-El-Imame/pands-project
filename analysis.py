23a910a18b11e891b53208cf693f9febb0b6567f
Maroua El Imame <g00472980@atu.ie>
2025-04-13 22:35:14 +0100

    Iris dataset downloaded & analysis.py created

---
+++ analysis.py
@@ -0,0 +1,18 @@
+# This program is part of the programming and Scripting module project in ATU.
+# This project concerns the well-known Fisher’s Iris dataset
+# Please refer to Readme file and Jupyter notebook for aditional information about Iris dataset research, analysis and visualization.
+
+# 1. Download and place the Iris into the current repository:
+                # https://archive.ics.uci.edu/dataset/53/iris
+
+# 2. Current file : analysis.py
+    # This program outputs a summary of each variable to a single text file
+    # Saves a histogram of each variable to png files
+    # Outputs a scatter plot of each pair of variables
+    # Performs any other analysis we think is appropriate.
+
+# scope : 
+    # Imagine we are giving a presentation on the Iris dataset where we explain what investigating a dataset entails and how Python can be used to do it.
+
+# Author : Maroua EL imame 
+
